bd95e7d71ad1fd27dc9833917736cbbaf921f721
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-17 14:51:51 -0400

    Updated main.cpp to use new window system

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,68 @@
 #include <GLFW/glfw3.h>
 #include <cstdlib>
 #include "graphics/window.h"
+#include "graphics/mesh.h"
 
 using namespace Fenrir;
 using namespace Fenrir::Graphics;
 int main()
 {
+
+  // std::cout << "Test";
   if(!glfwInit())
     exit(EXIT_FAILURE);
 
-  glewExperimental = GL_TRUE;
-  glewInit();
   Window* window = new Window(640, 480, "Fenrir");
+  glewExperimental=GL_TRUE;
+  GLenum err = glewInit();
+  std::cout << err << std::endl;
+  ShaderFactory::GetInstance()->LoadShaderFromFile("./shaders/shader.vert", "./shaders/shader.frag");
+
+  GLfloat vertices[] = {
+    -1.0f,-1.0f,-1.0f, // triangle 1 : begin
+    -1.0f,-1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f, // triangle 1 : end
+    1.0f, 1.0f,-1.0f, // triangle 2 : begin
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f, // triangle 2 : end
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    -1.0f,-1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    -1.0f,-1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f,-1.0f,
+    1.0f,-1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f,-1.0f,
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f,-1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f, 1.0f, 1.0f,
+    -1.0f, 1.0f, 1.0f,
+    1.0f,-1.0f, 1.0f
+};
+
+  Mesh* myCube = new Mesh(vertices, 36, ShaderFactory::GetInstance()->GetShaderAtIndex(0));
 
   while(window->IsRunning())
   {
+    myCube->DrawMesh();
+    window->Render();
     glfwPollEvents();
   }
 
