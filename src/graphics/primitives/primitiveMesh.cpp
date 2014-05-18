3c219611ca9971ea8dbb38aa0396f23dfaccc2cb
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-18 18:44:41 -0400

    Documented Primitive mesh

--- src/graphics/primitives/primitiveMesh.cpp
+++ src/graphics/primitives/primitiveMesh.cpp
@@ -3,6 +3,8 @@ using namespace Fenrir;
 using namespace Fenrir::Graphics;
 using namespace Fenrir::Graphics::Primitives;
 
+
+//Create a new primitive mesh that used the specified shaderProgram
 PrimitiveMesh::PrimitiveMesh(GLuint shaderProgram)
   : Mesh(&(this->vertexData->at(0)),
          this->vertexData->size()/3,
