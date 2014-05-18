6605a82e19e7ba1ef3d989c5c6bd6c9544b67ccd
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-18 18:35:34 -0400

    Documented Fenrir::Graphics::Window

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -4,12 +4,15 @@ using namespace Fenrir::Graphics;
 
 Window::Window(uint width, uint height, char* title)
 {
+  //Create a new GLFW window
   this->window = glfwCreateWindow(width, height, title, NULL, NULL);
   if(!window)
   {
     glfwTerminate();
     exit(EXIT_FAILURE);
   }
+
+  //Tell opengl that it will use this window to draw
   glfwMakeContextCurrent(this->window);
 }
 
@@ -25,11 +28,13 @@ bool Window::IsRunning()
 
 void Window::Clear(float r, float g, float b)
 {
+  //Clear the screen to the specified color value
   glClearColor(r, g, b, 1);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void Window::Render()
 {
+  //Blit everything!
   glfwSwapBuffers(this->window);
 }
