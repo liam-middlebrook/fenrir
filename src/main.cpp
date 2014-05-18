ccd4d52d96d42eaef20a0f85e1f30eef8ffd4efd
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-18 16:41:43 -0400

    Updated main.cpp to test out cube and cone

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 #include <cstdlib>
 #include "graphics/window.h"
 #include "graphics/mesh.h"
-
+#include "graphics/primitives/cube.h"
+#include "graphics/primitives/cone.h"
 using namespace Fenrir;
 using namespace Fenrir::Graphics;
+using namespace Fenrir::Graphics::Primitives;
 int main()
 {
 
@@ -59,7 +61,8 @@ int main()
     1.0f,-1.0f, 1.0f
 };
 
-  Mesh* myCube = new Mesh(vertices, 36, ShaderFactory::GetInstance()->GetShaderAtIndex(0));
+  //Mesh* myCube = new Mesh(vertices, 36, ShaderFactory::GetInstance()->GetShaderAtIndex(0));
+  Cone* myCube = new Cone(ShaderFactory::GetInstance()->GetShaderAtIndex(0));
   myCube->fillColor = glm::vec3(1,0,0);
   glm::mat4 viewMatrix = glm::lookAt(
                                      glm::vec3(3, 3, 5),
@@ -69,7 +72,7 @@ int main()
   while(window->IsRunning())
   {
     window->Clear(100/255.0f, 149/255.0f, 237/255.0f);
-    myCube->worldMatrix = glm::scale(glm::mat4(1), glm::vec3(0.5)) * glm::rotate(glm::mat4(1), (float)glfwGetTime(), glm::vec3(0, 1, 0));
+    myCube->worldMatrix = glm::scale(glm::mat4(1), glm::vec3(0.5)) * glm::rotate(glm::mat4(1), (float)glfwGetTime(), glm::vec3(0,1,1));
     myCube->DrawMesh(viewMatrix, projMatrix, true);
     window->Render();
     glfwPollEvents();
