112b797b17643116c4db463d019156cf514bc17e
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-18 13:18:02 -0400

    Added matrix and color uniforms to Fenrir::Graphics::Mesh

--- src/graphics/mesh.cpp
+++ src/graphics/mesh.cpp
@@ -10,13 +10,36 @@ Mesh::Mesh(GLfloat* vertices, uint vertexCount, GLuint shaderProgram)
 
   glGenBuffers(1, &this->vertexBuffer);
   glBindBuffer(GL_ARRAY_BUFFER, this->vertexBuffer);
-  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertexCount, vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(GLfloat) * vertexCount* 3, vertices, GL_STATIC_DRAW);
+  GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+  glEnableVertexAttribArray(posAttrib);
+  glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+  this->worldMatrix = glm::mat4(1);
+  this->fillColor = glm::vec3(1,1,1);
+  this->strokeColor = glm::vec3(0,0,0);
 }
 
-void Mesh::DrawMesh()
+void Mesh::DrawMesh(glm::mat4 viewMatrix, glm::mat4 projectionMatrix, bool outline)
 {
+  GLint viewUni = glGetUniformLocation(this->shaderProgram, "viewMatrix");
+  GLint projectionUni = glGetUniformLocation(this->shaderProgram, "projectionMatrix");
+  GLint worldUni = glGetUniformLocation(this->shaderProgram, "worldMatrix");
+
+  GLint colorUni = glGetUniformLocation(this->shaderProgram, "color");
+
+  glUniformMatrix4fv(viewUni, 1, GL_FALSE, glm::value_ptr(viewMatrix));
+  glUniformMatrix4fv(projectionUni, 1, GL_FALSE, glm::value_ptr(projectionMatrix));
+  glUniformMatrix4fv(worldUni, 1, GL_FALSE, glm::value_ptr(worldMatrix));
+
   glUseProgram(shaderProgram);
-  glDrawArrays(GL_TRIANGLES, 0, this->vertexCount);
+  glUniform3f(colorUni, this->fillColor.x, this->fillColor.y, this->fillColor.z);
+  glDrawArrays(GL_TRIANGLES, 0, this->vertexCount*3);
+
+  if(outline)
+  {
+    glUniform3f(colorUni, this->strokeColor.x, this->strokeColor.y, this->strokeColor.z);
+    glDrawArrays(GL_LINE_STRIP, 0, this->vertexCount*3);
+  }
 }
 
 Mesh::~Mesh()
--- src/graphics/mesh.h
+++ src/graphics/mesh.h
@@ -2,6 +2,9 @@
 #define MESH_H_
 
 #include "shaderFactory.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Fenrir
 {
@@ -12,7 +15,10 @@ namespace Fenrir
     public:
       Mesh(GLfloat* vertices, uint vertexCount, GLuint shaderProgram);
       ~Mesh();
-      void DrawMesh();
+      void DrawMesh(glm::mat4 viewMatrix, glm::mat4 projectionMatrix, bool drawOutline);
+      glm::mat4 worldMatrix;
+      glm::vec3 fillColor;
+      glm::vec3 strokeColor;
     protected:
     private:
       GLuint vertexBuffer;
