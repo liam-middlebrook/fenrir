e767ebffbf1dbec6341faf5bee8d25ef416d1297
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-18 18:08:09 -0400

    Added depth buffer support to window

--- src/graphics/window.cpp
+++ src/graphics/window.cpp
@@ -26,7 +26,7 @@ bool Window::IsRunning()
 void Window::Clear(float r, float g, float b)
 {
   glClearColor(r, g, b, 1);
-  glClear(GL_COLOR_BUFFER_BIT);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void Window::Render()
