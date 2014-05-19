bfb30964b4fcca549332c5b22f072e48ad3c40f1
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-05-18 21:58:17 -0400

    Implemented Tube Drawing

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "graphics/primitives/cube.h"
 #include "graphics/primitives/cone.h"
 #include "graphics/primitives/sphere.h"
+#include "graphics/primitives/tube.h"
 #include "graphics/primitives/cylinder.h"
 using namespace Fenrir;
 using namespace Fenrir::Graphics;
@@ -29,30 +30,30 @@ int main()
 
   Cube* myCube = new Cube(ShaderFactory::GetInstance()->GetShaderAtIndex(0));
   Cone* myCone = new Cone(ShaderFactory::GetInstance()->GetShaderAtIndex(0));
-  Cylinder* myCylinder = new Cylinder(ShaderFactory::GetInstance()->GetShaderAtIndex(0));
+  Tube* myCylinder = new Tube(ShaderFactory::GetInstance()->GetShaderAtIndex(0));
 
-  Sphere* mySphere = new Sphere(ShaderFactory::GetInstance()->GetShaderAtIndex(0));
+//  Sphere* mySphere = new Sphere(ShaderFactory::GetInstance()->GetShaderAtIndex(0));
 
 
   myCube->fillColor = glm::vec3(1,0,0);
   myCylinder->fillColor = glm::vec3(0,0,1);
-  mySphere->fillColor = glm::vec3(0,1,0);
+  //mySphere->fillColor = glm::vec3(0,1,0);
   myCone->fillColor = glm::vec3(0,1,0);
   glm::mat4 viewMatrix = glm::lookAt(
-                                     glm::vec3(3, 3, 5),
+                                     glm::vec3(3, 1, 8),
                                      glm::vec3(0, 0, 0),
                                      glm::vec3(0, 1, 0));
   glm::mat4 projMatrix = glm::perspective(45.0f, 4.0f/3.0f, 0.01f, 100.0f);
   while(window->IsRunning())
   {
     window->Clear(100/255.0f, 149/255.0f, 237/255.0f);
-    mySphere->worldMatrix = glm::scale(glm::mat4(1), glm::vec3(0.5))*glm::rotate(glm::mat4(1), (float)glfwGetTime(), glm::vec3(0, 0, 1));
+    //mySphere->worldMatrix = glm::scale(glm::mat4(1), glm::vec3(0.5))*glm::rotate(glm::mat4(1), (float)glfwGetTime(), glm::vec3(0, 0, 1));
     myCube->worldMatrix = glm::scale(glm::mat4(1), glm::vec3(0.25))*glm::translate(glm::mat4(1), glm::vec3(-5, 0, -4));
     myCone->worldMatrix = glm::scale(glm::mat4(1), glm::vec3(0.5)) * glm::rotate(glm::mat4(1), (float)glfwGetTime(), glm::vec3(1,1,1));
-    myCylinder->worldMatrix = glm::scale(glm::mat4(1), glm::vec3(0.5)) * glm::rotate(glm::mat4(1), (float)glfwGetTime(), glm::vec3(1,1,1)) * glm::translate(glm::mat4(1), glm::vec3(7,0,5));
-    mySphere->DrawMesh(viewMatrix, projMatrix, true);
+    myCylinder->worldMatrix = glm::scale(glm::mat4(1), glm::vec3(0.5)) * glm::rotate(glm::mat4(1), (float)glfwGetTime(), glm::vec3(0,1,0));
+    //mySphere->DrawMesh(viewMatrix, projMatrix, true);
    // myCube->DrawMesh(viewMatrix, projMatrix, true);
-   // myCylinder->DrawMesh(viewMatrix, projMatrix, true);
+    myCylinder->DrawMesh(viewMatrix, projMatrix, true);
    // myCone->DrawMesh(viewMatrix, projMatrix, true);
     window->Render();
     glfwPollEvents();
